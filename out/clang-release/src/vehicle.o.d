0479ffc5f8ee807df288d8f7c2564049d3cf0931
User <User@DESKTOP-HFLITHM>
2024-02-25 13:19:48 +0100

    Initial files

---
+++ out/clang-release/src/vehicle.o.d
@@ -0,0 +1,6 @@
+../out/clang-release/src/vehicle.o: vehicle.cc vehicle.h \
+  C:/Users/User/Downloads/omnetpp-6.0.2-windows-x86_64/omnetpp-6.0.2/include/omnetpp.h
+
+vehicle.h:
+
+C:/Users/User/Downloads/omnetpp-6.0.2-windows-x86_64/omnetpp-6.0.2/include/omnetpp.h:
